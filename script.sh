51baf2702ade28ba71f52ac48350671b596e9218
Kulbhushan Mayer <kulbhushan.mayer@thinknyx.com>
2020-04-28 13:36:03 +0530

    Fixing bug in last change done by Nikhitha

--- script.sh
+++ script.sh
@@ -5,3 +5,5 @@ echo "This is my first change"
 echo "This is change is being done from the GITHUB remote UI"
 echo "Demo for difference between Local & Remote Branches in Local Repo"
 echo "Nikhitha did this change"
+echo "Fixing bug in last change done by Nikhitha"
+
