a466252952c86a24fe2df1344d679217d2746e3f
Kulbhushan Mayer <kulbhushan.mayer@thinknyx.com>
2020-04-28 15:30:37 +0530

    stale PR demo

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ echo "Demo for difference between Local & Remote Branches in Local Repo"
 echo "Nikhitha did this change"
 echo "this change is done Nikhitha for new task assigned to her"
 echo "Fixing bug in last change done by Nikhitha"
-
+echo "This change is to show stale PR feature"
